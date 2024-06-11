f740c7306198479bb7c020151aaabd914f97fbe8
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2024-06-12 08:42:39 +0900

    Title: 변수 출력하기 3, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

--- 240612/변수 출력하기 3/outputing-variables-3.java
+++ 240612/변수 출력하기 3/outputing-variables-3.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int a=1;
         int b=2;
-        int c='C';
+        char c='C';
         System.out.println(a+"->"+b+"->"+c);
     }
 }

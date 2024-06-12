79b236432175a403123f97f74584a7a56c36e4e0
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2024-06-12 09:41:35 +0900

    Title: 달에서 무게 구하기, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240612/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int chu = 13;
+        double ratio= 0.165000;
+        System.out.printf("%d * %.6f = %.6f",chu, ratio, chu*ratio); 
+    }
+}

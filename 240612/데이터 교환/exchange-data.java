6418b6af3bf502f79de6263a287e70b44157b831
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2024-06-12 10:10:39 +0900

    Title: 데이터 교환, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240612/데이터 교환/exchange-data.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b= 6;
+        int c = 7;
+
+        //데이터 교환
+        int tmp = a;
+        a = c;
+        c = b;
+        b = tmp;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+}

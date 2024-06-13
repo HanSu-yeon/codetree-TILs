f5d4980140d20a09afb1066ee03215056acb0dcd
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2024-06-13 10:57:55 +0900

    Title: 삼항연산자, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240613/삼항연산자/ternary-operator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        
+        n = sc.nextInt();
+        
+        String res = (n>=100) ?  "pass" : "failure";
+        System.out.println(res);
+        
+    }
+}

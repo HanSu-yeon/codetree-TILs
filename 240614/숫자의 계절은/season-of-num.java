8fe84e215f545e42986015c73598caaf3c4609e0
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2024-06-14 10:08:50 +0900

    Title: 숫자의 계절은, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

--- 240614/숫자의 계절은/season-of-num.java
+++ 240614/숫자의 계절은/season-of-num.java
@@ -6,9 +6,9 @@ public class Main {
         
         int m = sc.nextInt();
         //3~5월이 봄, 6~8월이 여름, 9~11월이 가을, 12~2월이 겨울
-        if((m<=1 || m<=2)||m==12){
+        if(m>=12 || m<=2){
             System.out.println("Winter");
-        }else if(m>=3 || m<=5){
+        }else if(m<=5){
             System.out.println("Spring");
         }else if(m<=8){
             System.out.println("Summer");

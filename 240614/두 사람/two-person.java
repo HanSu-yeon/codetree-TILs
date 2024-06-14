efe760c2df391670cc1f83e0f6e88f2810ee22f1
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2024-06-14 11:14:23 +0900

    Title: 두 사람, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240614/두 사람/two-person.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a_age, b_age;
+        String a_s, b_s;
+        a_age = sc.nextInt();
+        a_s = sc.next();
+        b_age = sc.nextInt();
+        b_s = sc.next();
+
+        if((a_age>=19 || b_age>=19 ) && ("M".equals(a_s) || "M".equals(b_s))){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+
+        }
+    }
+}

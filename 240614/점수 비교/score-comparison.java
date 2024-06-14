d9112295d301852c54804c45c7ceacf241763124
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2024-06-14 09:28:27 +0900

    Title: 점수 비교, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240614/점수 비교/score-comparison.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a_math, a_eng,b_math,b_eng;
+        a_math = sc.nextInt();
+        a_eng = sc.nextInt();
+        b_math = sc.nextInt();
+        b_eng = sc.nextInt();
+        
+        if(a_math>b_math && a_eng>b_eng){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+
+    }
+}

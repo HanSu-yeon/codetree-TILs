dd887025e0fcaad847a386d6f11c2799b7c23992
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2024-06-17 09:30:45 +0900

    Title: 윤년인가, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240617/윤년인가/is-leap-year.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int y = sc.nextInt();
+
+        if(y%4==0 || (y%100!=0 && y%400==0)){
+            //윤년
+            System.out.println("true");
+        }else{
+            //평년
+            System.out.prinln("false");
+        }
+    }
+}

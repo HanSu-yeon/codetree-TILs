f3d5bbb02cbb3fd284922ee6506bf1722c22b516
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2026-01-05 15:07:19 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 73ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260105/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let n = Number(input[0]);
+// Please Write your code here.
+
+function printStar(n){
+    if(n === 0 ){
+        return;
+    }
+
+    for(let i = 0; i<n; i++){
+        process.stdout.write('*'+" ")
+    }
+    console.log()
+    printStar(n -1 );
+    for(let i = 0; i<n; i++){
+        process.stdout.write('*'+" ")
+    }
+    console.log()
+}
+
+printStar(n)

095eb5f27f606b1acd63bd160b424baeea2b6d2e
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2026-01-13 17:15:09 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 260113/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.js
+++ 260113/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.js
@@ -4,9 +4,8 @@ const n = Number(input[0]);
 
 // Please Write your code here.
 function f(n) {
-    if (n === 1) {
-        return 1
-    }
+    if (n === 1) return 1
+
     if (n === 2) return 2
     return f(n - 2) + n
 

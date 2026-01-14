35a8f7824f50f70ab5457e8d1a7f733cc6537f01
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2026-01-14 10:58:16 +0900

    Title: 큰 수 자리수의 합, Time: 75ms, Memory: 12MB, Status: Runtime Error - Codetree

---
+++ 260114/큰 수 자리수의 합/sum-of-large-numeric-digits.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let [a, b, c] = input[0].split(" ").map(Number);
+
+// Please Write your code here.
+function f(n){
+
+
+}
+
+
+let sumValue = a * b* c
+let arrVal = Array.from(String(sumValue))
+console.log(f(arrVal[]))

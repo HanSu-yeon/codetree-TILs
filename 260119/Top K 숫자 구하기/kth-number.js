e08f2868e0c43d75c55480edcf083b894507a085
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2026-01-19 23:38:45 +0900

    Title: Top K 숫자 구하기, Time: 64ms, Memory: 10MB, Status: Passed - Codetree

--- 260119/Top K 숫자 구하기/kth-number.js
+++ 260119/Top K 숫자 구하기/kth-number.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [N, K] = input[0].split(' ');
-const [arrN] = input[1]
+const nums = input[1].trim().split(' ').map(Number)
 
-arrN.sort((a, b)=> a-b)
-console.log(arrN)
+nums.sort((a, b)=> a-b)
+console.log(nums[K-1])

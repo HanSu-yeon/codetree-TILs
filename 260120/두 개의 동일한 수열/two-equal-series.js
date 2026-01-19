e9f969796c8680df44ea1801d2250aee8d4a4a30
HanSuyeon <71827641+HanSu-yeon@users.noreply.github.com>
2026-01-20 00:20:25 +0900

    Title: 두 개의 동일한 수열, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 260120/두 개의 동일한 수열/two-equal-series.js
+++ 260120/두 개의 동일한 수열/two-equal-series.js
@@ -1,30 +1,31 @@
 const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
+// 변수 선언 및 입력
 const n = Number(input[0]);
 const a = input[1].trim().split(' ').map(Number);
 const b = input[2].trim().split(' ').map(Number);
 
-// Please Write your code here.
-
-function equal(a, b) {
+// equal 함수 선언
+function equal() {
+    // n개의 원소를 순서대로 봤을 때
+    // 전부 동일한 경우에만 일치합니다.
+    // 단 하나라도 다르다면, false입니다.
     for (let i = 0; i < n; i++) {
         if (a[i] !== b[i]) {
-
-            return false
+            return false;
         }
-        return true
     }
-
+    return true;
 }
 
+// 정렬
+a.sort((a, b) => a - b);
+b.sort((a, b) => a - b);
 
-a.sort((a, b) => a - b)
-b.sort((a, b) => a - b)
-
+// 수열이 일치하는지 확인합니다.
 if (equal()) {
-    console.log("Yes")
+    console.log("Yes");
 } else {
-    console.log("No")
-
+    console.log("No");
 }
